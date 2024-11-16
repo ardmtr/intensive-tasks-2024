91d28037bf3a7d5b329e66bec6116e61b221fc07
ardmtr <ardmtr@gmail.com>
2024-11-17 02:38:28 +0300

    Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -26,11 +26,26 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+
+        int year = 4002;
+
+        System.out.println(isLeap(year));
+
     }
 
     static boolean isLeap(int year) {
         //Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+        if (year <= 0 ||year > Integer.MAX_VALUE) {
+            return false;
+        }
+
+        if (((year % 4 == 0 && year % 100 != 0)) || (year % 400 == 0)) {
+
+            return true;
+        }
+
+        return false;
+
     }
 }
