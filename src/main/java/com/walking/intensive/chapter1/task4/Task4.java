bb4a9b3207954716bed678fbbbfa7fc243d10996
ardmtr <ardmtr@gmail.com>
2024-11-17 10:13:28 +0300

    Task3

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,9 +25,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 1;
+        double b = -8;
+        double c = 15;
 
         System.out.println(solveEquation(a, b, c));
 
@@ -36,6 +36,68 @@ public class Task4 {
     static String solveEquation(double a, double b, double c) {
         //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0) {
+
+            if (b == 0) {
+
+                if (c == 0) {
+
+                    return "Бесконечное множество решений.";
+                } else {
+
+                    return "Количество решений: 0.";
+                }
+            } else {
+
+                double root = -c / b;
+
+                return "Количество решений: 1. Корень: " + root;
+
+            }
+        }
+
+        double sumOfRoots = -b / a;
+
+        double productOfRoots = c / a;
+
+        if (sumOfRoots == 0 && productOfRoots == 0) {
+
+            return "Количество решений: 1. Корень: 0";
+
+        } else if (productOfRoots == 0) {
+
+            double nonZeroRoot = sumOfRoots;
+
+            return "Количество решений: 2. Корни: 0;" + nonZeroRoot;
+
+        } else {
+
+            double discriminant = sumOfRoots * sumOfRoots - 4 * productOfRoots;
+
+            if (discriminant >= 0) {
+
+            }
+                double sqrtDiscriminant = Math.sqrt(discriminant);
+
+                double root1 = (sumOfRoots - sqrtDiscriminant) / 2;
+
+                double root2 = (sumOfRoots + sqrtDiscriminant) / 2;
+
+                if (root1 > root2) {
+
+                    double temp = root1;
+
+                    root1 = root2;
+
+                    root2 = temp;
+
+                }
+
+                return "Количество решений: 2. Корни: " + root1 + ";" + root2;
+
+            } else {
+
+                return "Количество решений: 0.";
+            }
+        }
     }
-}
