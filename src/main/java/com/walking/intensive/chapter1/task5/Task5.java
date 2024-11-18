3a2ac10ef5d4d0133393e4fe224d8923106a99e3
ardmtr <ardmtr@gmail.com>
2024-11-18 14:52:48 +0300

    Task5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Задача поиска площади, величин углов, длин высот, биссектрис, медиан, радиусов вписанной и описанной вокруг
  * треугольника окружностей является центральной в Геометрии.
@@ -11,8 +13,10 @@ package com.walking.intensive.chapter1.task5;
 public class Task5 {
     public static void main(String[] args) {
 
-        System.out.println(getAreaByHeron(-1,10,10));
-        System.out.println(getHeights(-1,10,10).toString());
+        // System.out.println(getAreaByHeron(-1,-2,-2));
+       // System.out.println(Arrays.toString(getHeights(2, 3, 4)));
+       // System.out.println(Arrays.toString(getMedians(4,5,6)));
+        System.out.println(Arrays.toString(getBisectors(3,4,5)));
     }
 
     /**
@@ -26,11 +30,11 @@ public class Task5 {
      */
     static double getAreaByHeron(double a, double b, double c) {
 
-        if (a + b <= c || a + c <= b || b + c <= a) {
+        if (isWrongTriangle(a, b, c)) {
             return -1;
         }
 
-        double halfPerimeter = (a + b +c) / 2;
+        double halfPerimeter = (a + b + c) / 2;
         return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 
@@ -44,16 +48,18 @@ public class Task5 {
     static double[] getHeights(double a, double b, double c) {
 
         double[] heightsArray;
-        if (a + b <= c || a + c <= b || b + c <= a) {
+        if (isWrongTriangle(a, b, c)) {
             return new double[0];
         }
 
+        double Area = getAreaByHeron(a, b, c);
         heightsArray = new double[3];
-        heightsArray[0] = 1;
-        heightsArray[1] = 2;
-        heightsArray[2] = 1;
+        heightsArray[0] = 2 * Area / a;
+        heightsArray[1] = 2 * Area / b;
+        heightsArray[2] = 2 * Area / c;
+     // Arrays.sort(heightsArray); так конечно проще =). но допустим мы про import ничего не знаем (да мы и не знаем, IDEA сама подсказала что и как)
 
-        return heightsArray;
+        return sortResult(heightsArray);
     }
 
     /**
@@ -65,8 +71,17 @@ public class Task5 {
      */
     static double[] getMedians(double a, double b, double c) {
         //        Место для вашего кода
+        if (isWrongTriangle(a, b, c)) {
+            return new double[0];
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double[] mediansArray;
+        mediansArray = new double[3];
+        mediansArray[0] = Math.sqrt(2 * a * a + 2 * b * b - c * c) / 2;
+        mediansArray[1] = Math.sqrt(2 * b * b + 2 * c * c - a * a) / 2;
+        mediansArray[2] = Math.sqrt(2 * c * c + 2 * a * a - b * b) / 2;
+
+        return sortResult(mediansArray);
     }
 
     /**
@@ -78,8 +93,17 @@ public class Task5 {
      */
     static double[] getBisectors(double a, double b, double c) {
         //        Место для вашего кода
+        if (isWrongTriangle(a, b, c)) {
+            return new double[0];
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double[] bisectorsArray;
+        bisectorsArray = new double[3];
+        bisectorsArray[0] = Math.sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b);
+        bisectorsArray[1] = Math.sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c);
+        bisectorsArray[2] = Math.sqrt(c * b * (a + b + c) * (c + b - a)) / (c + b);
+
+        return sortResult(bisectorsArray);
     }
 
     /**
@@ -91,6 +115,9 @@ public class Task5 {
      */
     static double[] getAngles(double a, double b, double c) {
         //        Место для вашего кода
+        if (isWrongTriangle(a, b, c)) {
+            return new double[0];
+        }
 
         return null; // Заглушка. При реализации - удалить
     }
@@ -105,6 +132,10 @@ public class Task5 {
     static double getInscribedCircleRadius(double a, double b, double c) {
         //        Место для вашего кода
 
+        if (isWrongTriangle(a, b, c)) {
+            return -1;
+        }
+
         return 0; // Заглушка. При реализации - удалить
     }
 
@@ -117,6 +148,9 @@ public class Task5 {
      */
     static double getCircumradius(double a, double b, double c) {
         //        Место для вашего кода
+        if (isWrongTriangle(a, b, c)) {
+            return -1;
+        }
 
         return 0; // Заглушка. При реализации - удалить
     }
@@ -137,7 +171,25 @@ public class Task5 {
      */
     static double getAreaAdvanced(double a, double b, double c) {
         //        Место для вашего кода
+        if (isWrongTriangle(a, b, c)) {
+            return -1;
+        }
 
         return 0; // Заглушка. При реализации - удалить
     }
-}
+
+    static boolean isWrongTriangle(double a, double b, double c) {
+        return (a + b <= c || a + c <= b || b + c <= a);
+    }
+
+    // сортировка из 3 чисел, без всяких пузырьков, вставок и тд
+    static double[] sortResult(double[] unSortResult) {
+        double [] sort;
+        sort = new double[3];
+        sort[0] = Math.min(Math.min(unSortResult[0], unSortResult[1]), unSortResult[2]);
+        sort[2] = Math.max(Math.max(unSortResult[0], unSortResult[1]), unSortResult[2]);
+        sort[1] = unSortResult[0] + unSortResult[1] + unSortResult[2] - sort[0] - sort[2];
+
+        return sort;
+    }
+ }
